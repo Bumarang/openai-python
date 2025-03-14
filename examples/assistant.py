0b59af0735f1bec3ca5ba29bd6663050a51ac2f3
Bumarang <info@bumarang.nl>
2025-03-14 09:10:20 +0100

    Update assistant.py

--- examples/assistant.py
+++ examples/assistant.py
@@ -4,8 +4,8 @@ import openai
 client = openai.OpenAI()
 
 assistant = client.beta.assistants.create(
-    name="Math Tutor",
-    instructions="You are a personal math tutor. Write and run code to answer math questions.",
+    name="Huurrecht specialist",
+    instructions="Je bent een huurrecht expert en geeft advies op grond van het woningwaarderingsstelsel",
     tools=[{"type": "code_interpreter"}],
     model="gpt-4-1106-preview",
 )
@@ -15,13 +15,13 @@ thread = client.beta.threads.create()
 message = client.beta.threads.messages.create(
     thread_id=thread.id,
     role="user",
-    content="I need to solve the equation `3x + 11 = 14`. Can you help me?",
+    content="Ik wil de maximale huurprijs berekenen van een onzelfstandige woning 10m2 slaapkamer eigen wastafel gemeenschappelijke keuken en woonkamer 5 medebewoners gedeelde douche en wc fietsenberging en eigen balkon 3m2`. Kun je me helpen?",
 )
 
 run = client.beta.threads.runs.create_and_poll(
     thread_id=thread.id,
     assistant_id=assistant.id,
-    instructions="Please address the user as Jane Doe. The user has a premium account.",
+    instructions="Please address the user as Mathijs Betten. The user has a premium account.",
 )
 
 print("Run completed with status: " + run.status)
